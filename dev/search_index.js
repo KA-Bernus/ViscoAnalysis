a5b6b5641ff982820e1565e9ad09d182027684c9
Documenter.jl <documenter@juliadocs.github.io>
2023-08-08 12:32:06 +0000

    build based on 32e6d1c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ViscoAnalysis","category":"page"},{"location":"#ViscoAnalysis","page":"Home","title":"ViscoAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ViscoAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ViscoAnalysis]","category":"page"}]
+}
